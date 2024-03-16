3ee28ea29bdd00d3c048703a6a8378b32cc463df
Sovan Roy <roysovan00@gmail.com>
2024-03-16 13:13:44 +0530

    added FeaturedSection

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,11 @@
 import Herosection from "@/components/Herosection";
+import FeaturedCourses from "@/components/FeaturedCourses";
 
 export default function HomePage() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <Herosection/>
+      <FeaturedCourses/>
     </main>
   );
 }
