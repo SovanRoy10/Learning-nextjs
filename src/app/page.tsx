aa01e7440494b6f83018f4f70297e191db2e7a0a
Sovan Roy <roysovan00@gmail.com>
2024-03-16 13:27:41 +0530

    added sticky-scroll-reveal

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,13 @@
 import Herosection from "@/components/Herosection";
 import FeaturedCourses from "@/components/FeaturedCourses";
+import WhyChooseUs from "@/components/WhyChooseUs";
 
 export default function HomePage() {
   return (
     <main className="min-h-screen bg-black/[0.96] antialiased bg-grid-white/[0.02]">
       <Herosection/>
       <FeaturedCourses/>
+      <WhyChooseUs/>
     </main>
   );
 }
